f335fe57b11cbd57afcfc91190c9f984ca904edd
Sathya <sathyaraj11127@gmail.com>
2021-08-14 22:24:44 +0530

    upload code

---
+++ milestone1/Activity13.java
@@ -0,0 +1,10 @@
+package milestone1;
+
+public class Activity13 {
+	public static void main(String[] args) {
+		int a = 23;
+		int p = 206;
+		int b = p/2;
+		System.out.println("Area "+((a+b)/2)*(b-((a+b)/2))+"sq.m.");
+	}
+}
