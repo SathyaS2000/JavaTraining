8fbaba1e4d5d79926ef724aee1890279eeb2cedd
Sathya <sathyaraj11127@gmail.com>
2021-08-14 18:35:54 +0530

    updated activity8

--- milestone1/Activity8.java
+++ milestone1/Activity8.java
@@ -4,5 +4,12 @@ import java.util.*;
 public class Activity8{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of the train");
+		double len = sc.nextDouble();
+		System.out.println("Enter the time taken to pass the man");
+		double sec = sc.nextDouble();
+		double relspeed = (len/sec*18.0/5.0);
+		double speed = relspeed + 5;
+		System.out.println("The speed of the train is "+speed+"kmph");
 	}
 }
